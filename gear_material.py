13006099878b7b13dd196241424f5766b64ef0c3
anida16 <avdalvi2@gmail.com>
2021-04-05 12:53:06 +0530

    Added gear material

---
+++ gear_material.py
@@ -0,0 +1,20 @@
+#Gear Material Database
+
+CI_Grade_20_Tensile = 500
+CI_Grade_25_Tensile = 600
+CI_Grade_35_Tensile = 600
+CI_Grade_35_Heat_Treated_Tensile = 750
+
+Steel_C45_Tensile = 500
+Steel_15Ni2Cr1Mo15_Tensile = 950
+Steel_40Ni2Cr1Mo28_Tensile = 1100 #Rukhande Selected this, so in tkinter, keep default value as this @Anthony
+
+
+CI_Grade_20_Bending = 50
+CI_Grade_25_Bending = 60
+CI_Grade_35_Bending = 60
+CI_Grade_35_Heat_Treated_Bending = 80
+
+Steel_C45_Bending = 140
+Steel_15Ni2Cr1Mo15_Bending = 320
+Steel_40Ni2Cr1Mo28_Bending = 400 #Rukhande Selected this, so in tkinter, keep default value as this @Anthony
