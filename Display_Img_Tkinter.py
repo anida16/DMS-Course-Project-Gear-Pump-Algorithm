0101e668d9a39f49b99802e254655204b1bd35e0
Anthony D'souza <jordananthony786@gmail.com>
2021-04-06 02:38:39 +0530

    For Reference Purpose
    For Displaying images and Graph plotting in Tkinter GUI refer to these two programs.

---
+++ Display_Img_Tkinter.py
@@ -0,0 +1,13 @@
+'''
+# Code for Displaying Images in Tkinter
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+
+img = ImageTk.PhotoImage(Image.open(r"Transmission Unit.png"))
+myLabel = Label(image=img)
+myLabel.pack()
+
+root.mainloop()
+'''
